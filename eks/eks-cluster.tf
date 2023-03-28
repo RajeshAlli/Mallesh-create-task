f1e01ec8e1d728913c7791be2e9f359147967438
Rajesh Alli <99278839+RajeshAlli@users.noreply.github.com>
2023-03-28 10:17:08 +0530

    a

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -13,21 +13,21 @@ module "eks" {
   
 worker_groups = [
     {
-      name                          = "frontend-worker-group"
+      name                          = "frontend"
       instance_type                 = "t2.micro"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
     },
     {
-      name                          = "backend-java-worker-group"
+      name                          = "backend-java"
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
    {
-      name                          = "database-worker-group"
+      name                          = "database"
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
