84203f04089685257d667c1e1f4abd042040be8a
Rajesh-Alli <arajesh.alli@gmail.com>
2023-03-30 19:05:19 +0530

    A

--- Ec2/ec2.tf
+++ Ec2/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example1" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.large"
   key_name      = "j-d-k"
-  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  subnet_id     = "subnet-0cb12a10f32d346f6"
   vpc_security_group_ids = ["sg-0aaefcec063503097"]
   
   tags = {
@@ -20,7 +20,7 @@ resource "aws_instance" "example2" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.large"
   key_name      = "j-d-k"
-  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  subnet_id     = "subnet-0cb12a10f32d346f6"
   vpc_security_group_ids = ["sg-0aaefcec063503097"]
   
   tags = {
@@ -32,7 +32,7 @@ resource "aws_instance" "example3" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.large"
   key_name      = "j-d-k"
-  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  subnet_id     = "subnet-0cb12a10f32d346f6"
   vpc_security_group_ids = ["sg-0aaefcec063503097"]
   
   tags = {
@@ -45,7 +45,7 @@ resource "aws_instance" "example4" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.large"
   key_name      = "j-d-k"
-  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  subnet_id     = "subnet-0cb12a10f32d346f6"
   vpc_security_group_ids = ["sg-0aaefcec063503097"]
   
   tags = {
