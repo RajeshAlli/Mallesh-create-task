dad3cd7b82487f81d5cd5dd4f723c9e0bc740d6a
Rajesh-Alli <arajesh.alli@gmail.com>
2023-03-30 19:03:22 +0530

    A

--- Ec2/ec2.tf
+++ Ec2/ec2.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "example3" {
 
 resource "aws_instance" "example4" {
   ami           = "ami-002070d43b0a4f171"
-  instance_type = "t2.small"
+  instance_type = "t2.large"
   key_name      = "j-d-k"
   subnet_id     = "subnet-0d7c18bcef7df8b72"
   vpc_security_group_ids = ["sg-0aaefcec063503097"]
