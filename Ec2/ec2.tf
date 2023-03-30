ce6a4feb908793eea1071b1b0333897b1ab11e1b
Rajesh-Alli <arajesh.alli@gmail.com>
2023-03-30 18:26:08 +0530

    ec2

---
+++ Ec2/ec2.tf
@@ -0,0 +1,55 @@
+
+provider "aws" {
+  region = "us-east-1"
+} 
+
+resource "aws_instance" "example1" {
+  ami           = "ami-002070d43b0a4f171"
+  instance_type = "t2.large"
+  key_name      = "j-d-k"
+  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  vpc_security_group_ids = ["sg-055adaa05aaa241d0"]
+  
+  tags = {
+    Name = "jenkins"
+  }
+}
+ 
+
+resource "aws_instance" "example2" {
+  ami           = "ami-002070d43b0a4f171"
+  instance_type = "t2.large"
+  key_name      = "j-d-k"
+  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  vpc_security_group_ids = ["sg-055adaa05aaa241d0"]
+  
+  tags = {
+    Name = "kubernetes"
+  }
+}
+
+resource "aws_instance" "example3" {
+  ami           = "ami-002070d43b0a4f171"
+  instance_type = "t2.large"
+  key_name      = "j-d-k"
+  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  vpc_security_group_ids = ["sg-055adaa05aaa241d0"]
+  
+  tags = {
+    Name = "docker"
+  }
+}
+
+
+resource "aws_instance" "example4" {
+  ami           = "ami-002070d43b0a4f171"
+  instance_type = "t2.large"
+  key_name      = "j-d-k"
+  subnet_id     = "subnet-0d7c18bcef7df8b72"
+  vpc_security_group_ids = ["sg-055adaa05aaa241d0"]
+  
+  tags = {
+    Name = "nexus"
+  }
+}
+
