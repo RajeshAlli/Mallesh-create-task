77c35bab8cdb4af92377e4b9dc56525b7dd434d1
Rajesh-Alli <arajesh.alli@gmail.com>
2023-03-30 22:04:40 +0530

    aa

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "devops13-eks-${random_string.suffix.result}"
+  cluster_name = "devOps111-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
