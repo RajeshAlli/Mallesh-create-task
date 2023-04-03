37d0d8f3f3c4a36828ffe1b3c72cc65bbf5891d5
Rajesh Alli <99278839+RajeshAlli@users.noreply.github.com>
2023-04-03 15:52:33 +0530

    Update EmployeeService.js
    a

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://a49001f566bd74f478de643f7f1440b8-1016373134.us-east-1.elb.amazonaws.com:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://aca2d4221178d40759a64e493d7322ec-962846401.us-east-1.elb.amazonaws.com:8080/api/v1/employees";
 
 class EmployeeService {
 
